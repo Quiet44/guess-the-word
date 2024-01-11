c62771d24b3b3f477b55e93e7e65c0d80d520c80
Jason <133819475+Quiet44@users.noreply.github.com>
2024-01-11 11:55:27 -0800

    Accept and validate player guesses

--- guess-the-word-main/js/script.js
+++ guess-the-word-main/js/script.js
@@ -8,6 +8,7 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again-button");
 
 const word = "magnolia";
+const guessedLetters = [];
 
 const placeholder =function (word) {
     const placeholderLetters = [];
@@ -16,4 +17,35 @@ const placeholder =function (word) {
         placeholderLetters.push("@");
     }
     wordInProgress.innerText = placeholderLetters.join("");
+};
+
+guessButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    // Empty message paragraph
+    message.innerText = "";
+    // Let's grab what was entered in the input
+    const guess = letterInput.value;
+    //Let's make sure that it is a single letter
+    const goodGuess = validateInpute(guess);
+
+ if (goodGuess) {
+    // We've got a letter! Let's guess!
+    makeGuess(guess);
+ }
+});
+const validateInpute =function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length ===0) {
+        // Is the input empty?
+        message.innerText = "Please enter a letter.";
+    } else if (input.length > 1) {
+        // Did you type more than one letter?
+        message.innerText = "Please enter a single letter.";
+    } else if (input.match(acceptedLetter)) {
+        // Did you type a number, a special character or some other non letter thing?
+        message.innerText = "Please enter a letter from A to Z.";
+    } else {
+        // We finnally got a single letter, omg yay
+        return input;
+    }
 };
